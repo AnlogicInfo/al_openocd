eab670070d86e3f7bff8656b55dd92f71d791390
tywang <timwang1104@126.com>
2023-03-24 10:23:16 +0800

    add x1 loader

---
+++ contrib/loaders/flash/qspi/dwcssi/src/dwcssi_async_x1.c
@@ -0,0 +1,36 @@
+#include "dwcssi.h"
+
+int flash_dwcssi(volatile uint32_t *ctrl_base, int32_t page_size, int count, uint32_t *buf_start, uint32_t *buf_end, uint32_t offset, uint32_t prog_cmd, uint32_t addr_size)
+{
+    uint8_t *rp;
+    uint32_t retval = 0;
+    // uint32_t page_offset = offset & (page_size - 1);
+    uint32_t cur_count;
+
+    while(count > 0)
+    {
+        // if(page_offset + count > page_size)
+        //     cur_count = page_size - page_offset;
+        // else
+        //     cur_count = count;        
+        rp = (uint8_t *) wait_fifo(buf_start);
+        if(count < page_size)
+            cur_count = count;
+        else
+            cur_count = page_size;
+
+        retval = dwcssi_write_buffer_x1(ctrl_base, rp, offset, cur_count, prog_cmd, addr_size);
+
+        // page_offset = 0;
+        rp += cur_count;
+        if(rp == buf_end)
+            rp = buf_start + 2;
+        *(buf_start + 1) = (uint32_t) rp;
+
+        offset += cur_count;
+        count  -= cur_count;
+    }
+
+    return retval;
+}
+
