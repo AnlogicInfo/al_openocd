acceed0f2107a1ed279fceeeac20273c23baffcb
Tianyi Wang <tianyi.wang@anlogic.com>
2023-08-10 14:12:07 +0800

    work around for al9000 step
    Change-Id: Icefa5fa046120ec32a030160fa83908c1a9f4618
    
    Signed-off-by: Tianyi Wang <tianyi.wang@anlogic.com>

--- src/target/aarch64.c
+++ src/target/aarch64.c
@@ -661,8 +661,11 @@ static int aarch64_prepare_restart_one(struct target *target)
 	 * get passed along to all PEs. Also close gate for channel 0
 	 * to isolate the PE from halt events.
 	 */
-	if (retval == ERROR_OK)
-		retval = arm_cti_ungate_channel(armv8->cti, 1);
+	/*
+	 *workaround for al9000
+	 *if (retval == ERROR_OK)
+	 *	retval = arm_cti_ungate_channel(armv8->cti, 1);
+	 */
 	if (retval == ERROR_OK)
 		retval = arm_cti_gate_channel(armv8->cti, 0);
 
