99ac6d78e001b186788c56c91871c8ddc0009a6e
tywang <tywang@anlogic.com>
2025-07-04 15:54:57 +0800

    update ftdi_scan tcl

--- tcl/ftdi_scan.tcl
+++ tcl/ftdi_scan.tcl
@@ -1,13 +1,15 @@
-proc scan_device {port pid vid} {
-  puts "scan port $port"
-  adapter driver ftdi
-  adapter usb location $port
-  adapter speed    10000
-  ftdi vid_pid $pid $vid
-  ftdi layout_init 0x0008 0x001b
-  transport select jtag
-  jtag init
-  shutdown
+proc scan_device {pid vid {port ""}} {
+    adapter driver ftdi
+    if {$port ne ""} {
+      puts "scan port $port"
+      adapter usb location $port
+    }
+    adapter speed    10000
+    ftdi vid_pid $pid $vid
+    ftdi layout_init 0x0008 0x001b
+    transport select jtag
+    jtag init
+    shutdown
 }
 
 proc usb_list {pid vid} {
